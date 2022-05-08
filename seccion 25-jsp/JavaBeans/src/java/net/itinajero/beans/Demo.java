9d22888c5098876bbc4e1c61acd4bce763213f40
Kanec Hernandez Chim <99061515+KanecHdez@users.noreply.github.com>
2022-05-07 22:53:07 -0500

    Sección 25 - JSP Ejercicio Java Beans
    Sección 25 Java EE - JSP- Ejercicio Uso de Java Beans

---
+++ seccion 25-jsp/JavaBeans/src/java/net/itinajero/beans/Demo.java
@@ -0,0 +1,31 @@
+package net.itinajero.beans;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Demo extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Producto p = new Producto();
+        p.setDescripcion("Television");
+        p.setPrecioVenta(1524);
+
+        // Compartimos el Javabean en el alcance de request
+        request.setAttribute("producto", p);
+
+        // Compartimos el Javabean en el alcance de session. 
+        // Estara disponible mientras la session este activa.
+        // Puedes ver este atributo en el administrador de aplicaciones de Tomcat.
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("producto", p);
+
+        getServletContext().getRequestDispatcher("/display.jsp").forward(request, response);
+    }
+
+}
